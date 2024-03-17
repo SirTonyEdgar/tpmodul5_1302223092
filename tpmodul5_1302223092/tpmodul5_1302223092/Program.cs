645098daa171108feaaf1675492b5d87abf7de92
SirTonyEdgar <121780770+SirTonyEdgar@users.noreply.github.com>
2024-03-17 22:20:18 +0700

    Add files via upload

---
+++ tpmodul5_1302223092/tpmodul5_1302223092/Program.cs
@@ -0,0 +1,26 @@
+﻿public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> dataString = new DataGeneric<string>("1234567890");
+        dataString.PrintData();
+
+        DataGeneric<int> dataInt = new DataGeneric<int>(123456789);
+        dataInt.PrintData();
+    }
+}
